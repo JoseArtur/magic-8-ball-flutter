ff40578cd08fabda4c5ab7ad3a9da1888b1c2499
José Artur <65734671+JoseArtur@users.noreply.github.com>
2020-07-07 20:09:18 -0300

    Redesigning Magic 8 ball App
    So I made an variant of  the classic Magic 8 ball with the genie, I hope you enjoy it

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow[600],
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Ball(),
       ),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballnumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {setState(() {
+           ballnumber = Random().nextInt(4)+2;
+        });
+        },
+        child: Image.asset(
+          'images/genie$ballnumber.png',
+        ),
+      ),
+    );
+  }
+}
